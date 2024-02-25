75c2d6dd65160c3afa05758ad0e618165df092d6
JuanitoAlcachofa123 <juanchavezMGmaster@gmail.com>
2024-02-25 12:51:11 -0400

    migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->id();//INTEGER UNSIGNED INCREMENT
+            $table->string('name');//VARCHAR
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
