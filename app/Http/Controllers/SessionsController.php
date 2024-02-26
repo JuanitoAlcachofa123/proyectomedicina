a78cbeaa14d8ae2a1195a17dce501b970d891b54
JuanitoAlcachofa123 <juanchavezMGmaster@gmail.com>
2024-02-26 10:57:10 -0400

    hola

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SessionsController extends Controller
 {
-    //
+    public function create(){
+        return view('auth.login');
+    }
+
+    public function store(){
+
+        if(auth()->attempt(request(['email', 'password']))==false){
+            return back()->withErrors([
+                'message' => 'El email o contraseña son incorrectas, por favor vuelva a intentarlo'            
+            ]);
+        }
+        return redirect()->to('/');
+    }
+
+    public function destroy(){
+
+        auth()->logout();
+
+        return redirect()->to('/');
+    }
 }
